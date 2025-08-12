8f6abbb0e782ebb575c7146b99bdbca3c8202549
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-12 16:16:23 +0200

    Updated imports for new structure

--- anba4/__init__.py
+++ anba4/__init__.py
@@ -1,13 +1,10 @@
-from anba4.anba_model import initialize_anba_model
-from anba4.fe_basis import initialize_fe_basis
-from anba4.solvers import compute_stiffness, compute_inertia, stress_field, strain_field
-from anba4.anba_model_singular import initialize_anba_model_singular
-from anba4.fe_basis_singular import initialize_fe_basis_singular
-from anba4.solvers_singular import (
-    compute_stiffness_singular,
-    compute_inertia_singular,
-    stress_field_singular,
-    strain_field_singular,
-)
+from anba4.data.anba_model import initialize_anba_model
+from anba4.fea.fe_functions import initialize_fe_functions
+from anba4.fea.chains import initialize_chains
+from anba4.solvers.stiffness import compute_stiffness
+from anba4.solvers.inertia import compute_inertia
+from anba4.solvers.stress import stress_field
+from anba4.solvers.strain import strain_field
 from anba4 import voight_notation, material
 from anba4.utils import *
+from anba4.data_model import AnbaData
