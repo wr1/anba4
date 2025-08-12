4e8207f6cba6b893abff4876960d37ae8801cdd4
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-12 16:16:23 +0200

    Moved utils from anba_model to utils

--- anba4/utils.py
+++ anba4/utils.py
@@ -1,4 +1,19 @@
 import numpy as np
+from dolfin import (
+    as_vector,
+    as_tensor,
+    grad,
+    stressVectorToStressTensor,
+    as_matrix,
+    LocalSolver,
+    Function,
+    TrialFunction,
+    TestFunction,
+    inner,
+    dx,
+    strainVectorToStrainTensor,
+    strainTensorToStrainVector,
+)
 
 
 def ComputeShearCenter(stiff_matrix):
@@ -47,3 +62,90 @@ def PrincipalAxesRotationAngle(decoupled_stiff_matrix):
     else:
         angle = -np.arcsin(v3[0, 1])
     return angle
+
+
+def pos3d(POS):
+    "Return node coordinates Vector."
+    return as_vector([POS[0], POS[1], 0.0])
+
+
+def grad3d(u, up):
+    "Return 3d gradient."
+    g = grad(u)
+    return as_tensor(
+        [
+            [g[0, 0], g[0, 1], up[0]],
+            [g[1, 0], g[1, 1], up[1]],
+            [g[2, 0], g[2, 1], up[2]],
+        ]
+    )
+
+
+def epsilon(u, up):
+    "Return symmetric 3D infinitesimal strain tensor."
+    g3 = grad3d(u, up)
+    return 0.5 * (g3.T + g3)
+
+
+def rotated_epsilon(data, u, up):
+    "Return symmetric 3D infinitesimal strain tensor rotated into material reference."
+    eps = epsilon(u, up)
+    rot = data.MaterialRotation_matrix
+    rotMatrix = as_matrix(
+        (
+            (rot[0], rot[1], rot[2], rot[3], rot[4], rot[5]),
+            (rot[6], rot[7], rot[8], rot[9], rot[10], rot[11]),
+            (rot[12], rot[13], rot[14], rot[15], rot[16], rot[17]),
+            (rot[18], rot[19], rot[20], rot[21], rot[22], rot[23]),
+            (rot[24], rot[25], rot[26], rot[27], rot[28], rot[29]),
+            (rot[30], rot[31], rot[32], rot[33], rot[34], rot[35]),
+        )
+    )
+    roteps = strainVectorToStrainTensor(rotMatrix.T * strainTensorToStrainVector(eps))
+    return roteps
+
+
+def sigma_helper(mod, u, up):
+    "Return second Piola-Kirchhoff stress tensor."
+    et = epsilon(u, up)
+    ev = strainTensorToStrainVector(et)
+    elasticMatrix = as_matrix(
+        (
+            (mod[0], mod[1], mod[2], mod[3], mod[4], mod[5]),
+            (mod[6], mod[7], mod[8], mod[9], mod[10], mod[11]),
+            (mod[12], mod[13], mod[14], mod[15], mod[16], mod[17]),
+            (mod[18], mod[19], mod[20], mod[21], mod[22], mod[23]),
+            (mod[24], mod[25], mod[26], mod[27], mod[28], mod[29]),
+            (mod[30], mod[31], mod[32], mod[33], mod[34], mod[35]),
+        )
+    )
+    sv = elasticMatrix * ev
+    st = stressVectorToStressTensor(sv)
+    return st
+
+
+def Sigma(data, u, up):
+    "Return second Piola-Kirchhoff stress tensor."
+    return sigma_helper(data.modulus, u, up)
+
+
+def RotatedSigma(data, u, up):
+    "Return second Piola-Kirchhoff stress tensor."
+    return sigma_helper(data.RotatedStress_modulus, u, up)
+
+
+def local_project(v, V, u=None):
+    """Element-wise projection using LocalSolver"""
+    dv = TrialFunction(V)
+    v_ = TestFunction(V)
+    a_proj = inner(dv, v_) * dx
+    b_proj = inner(v, v_) * dx
+    solver = LocalSolver(a_proj, b_proj)
+    solver.factorize()
+    if u is None:
+        u = Function(V)
+        solver.solve_local_rhs(u)
+        return u
+    else:
+        solver.solve_local_rhs(u)
+        return
