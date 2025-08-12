8460ef4cf42ce11919a685d17253dd83780b9cb3
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-12 23:31:29 +0200

    Updated anbax_principal_axes.py to new ANBA4 syntax

--- examples/anbax_principal_axes.py
+++ examples/anbax_principal_axes.py
@@ -21,16 +21,13 @@
 # ----------------------------------------------------------------------
 #
 
-from dolfin import *
-
-# from dolfin import compile_extension_module
+import dolfin as df
 import numpy as np
-
 from anba4 import *
 import mshr
 
-parameters["form_compiler"]["optimize"] = True
-parameters["form_compiler"]["quadrature_degree"] = 2
+df.parameters["form_compiler"]["optimize"] = True
+df.parameters["form_compiler"]["quadrature_degree"] = 2
 
 # Basic material parameters. 9 is needed for orthotropic materials.
 
@@ -41,15 +38,15 @@ matMechanicProp = [E, nu]
 # Meshing domain.
 
 thickness = 0.1
-Square1 = mshr.Rectangle(Point(0.0, -1.0, 0.0), Point(1.0, 1.0, 0.0))
+Square1 = mshr.Rectangle(df.Point(0.0, -1.0, 0.0), df.Point(1.0, 1.0, 0.0))
 Square2 = mshr.Rectangle(
-    Point(thickness, -1 + thickness, 0), Point(2.0, 1.0 - thickness, 0)
+    df.Point(thickness, -1 + thickness, 0), df.Point(2.0, 1.0 - thickness, 0)
 )
 C_shape = Square1 - Square2
 mesh = mshr.generate_mesh(C_shape, 64)
 rot_angle = 30.0 / 180.0 * np.pi
-cr = cos(rot_angle)
-sr = sin(rot_angle)
+cr = np.cos(rot_angle)
+sr = np.sin(rot_angle)
 rot_tensor = np.array([[cr, -sr], [sr, cr]])
 mesh.coordinates()[:] = (rot_tensor @ mesh.coordinates().T).T
 mesh.coordinates()[:] += np.array([3, 1])
@@ -61,31 +58,36 @@ mesh.coordinates()[:] += np.array([3, 1])
 
 
 # CompiledSubDomain
-materials = MeshFunction("size_t", mesh, mesh.topology().dim())
-fiber_orientations = MeshFunction("double", mesh, mesh.topology().dim())
-plane_orientations = MeshFunction("double", mesh, mesh.topology().dim())
+materials = df.MeshFunction("size_t", mesh, mesh.topology().dim())
+fiber_orientations = df.MeshFunction("double", mesh, mesh.topology().dim())
+plane_orientations = df.MeshFunction("double", mesh, mesh.topology().dim())
 
 materials.set_all(0)
 fiber_orientations.set_all(0.0)
 plane_orientations.set_all(90.0)
 
 # Build material property library.
-mat1 = material.IsotropicMaterial(matMechanicProp, 1.0)
+mat1 = IsotropicMaterial(matMechanicProp, 1.0)
 
 matLibrary = []
 matLibrary.append(mat1)
 
-anba = anbax(mesh, 2, matLibrary, materials, plane_orientations, fiber_orientations)
-stiff = anba.compute()
+anbax_data = initialize_anba_model(
+    mesh, 2, matLibrary, materials, plane_orientations, fiber_orientations
+)
+initialize_fe_functions(anbax_data)
+initialize_chains(anbax_data)
+stiff = compute_stiffness(anbax_data)
 print("Stiff:")
 stiff.view()
 
-mass = anba.inertia()
+mass = compute_inertia(anbax_data)
 print("Mass:")
 mass.view()
 
 
-decoupled_stiff = DecoupleStiffness(stiff)
+stiff_mat = stiff.getValues(range(6), range(6))
+decoupled_stiff = DecoupleStiffness(stiff_mat)
 print("Decoupled Stiff:")
 print(decoupled_stiff)
 angle = PrincipalAxesRotationAngle(decoupled_stiff)
