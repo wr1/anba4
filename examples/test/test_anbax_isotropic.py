e087e210da69d9fb2dbed0ffd377cfc23e73fe83
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-12 15:47:04 +0200

    Updated to use split files

--- examples/test/test_anbax_isotropic.py
+++ examples/test/test_anbax_isotropic.py
@@ -1,22 +1,19 @@
 import unittest
 import numpy as np
-import numpy.testing as npt
 from dolfin import *
 from anba4 import *
 
 
 class Test_Isotropic(unittest.TestCase):
-    
     def testStiffMass(self):
-
         parameters["form_compiler"]["optimize"] = True
         parameters["form_compiler"]["quadrature_degree"] = 2
 
         # Basic material parameters. 9 is needed for orthotropic materials.
 
-        E = 1.
+        E = 1.0
         nu = 0.33
-        #Assmble into material mechanical property Matrix.
+        # Assmble into material mechanical property Matrix.
         matMechanicProp = [E, nu]
         # Meshing domain.
 
@@ -33,42 +30,152 @@ class Test_Isotropic(unittest.TestCase):
         plane_orientations.set_all(90.0)
 
         # Build material property library.
-        mat1 = material.IsotropicMaterial(matMechanicProp, 1.)
+        mat1 = material.IsotropicMaterial(matMechanicProp, 1.0)
 
         matLibrary = []
         matLibrary.append(mat1)
 
-        anba = anbax(mesh, 2, matLibrary, materials, plane_orientations, fiber_orientations)
-        stiff = anba.compute()
+        anbax_data = initialize_anba_model(
+            mesh, 2, matLibrary, materials, plane_orientations, fiber_orientations
+        )
+        initialize_fe_basis(anbax_data)
+        stiff = compute_stiffness(anbax_data)
         stiff.view()
 
-        mass = anba.inertia()
+        mass = compute_inertia(anbax_data)
         mass.view()
 
-        stress_result_file = XDMFFile('Stress.xdmf')
-        stress_result_file.parameters['functions_share_mesh'] = True
-        stress_result_file.parameters['rewrite_function_mesh'] = False
+        stress_result_file = XDMFFile("Stress.xdmf")
+        stress_result_file.parameters["functions_share_mesh"] = True
+        stress_result_file.parameters["rewrite_function_mesh"] = False
         stress_result_file.parameters["flush_output"] = True
 
-        anba.stress_field([1., 0., 0.,], [0., 0., 0.], "local", "paraview")
-        stress_result_file.write(anba.STRESS, t = 0.)
+        stress = stress_field(
+            anbax_data,
+            [
+                1.0,
+                0.0,
+                0.0,
+            ],
+            [0.0, 0.0, 0.0],
+            "local",
+            "paraview",
+        )
+        stress_result_file.write(stress, t=0.0)
 
+        np.testing.assert_almost_equal(
+            stiff.getValues(range(6), range(6)),
+            np.array(
+                [
+                    [
+                        3.11064401e-01,
+                        -5.76267647e-07,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        2.19833734e-16,
+                    ],
+                    [
+                        -5.76267647e-07,
+                        3.11064401e-01,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        -2.66259961e-16,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        1.00000000e00,
+                        5.13607660e-16,
+                        -3.11112792e-16,
+                        0.00000000e00,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        4.93713695e-16,
+                        8.33333333e-02,
+                        -8.92869184e-17,
+                        0.00000000e00,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        -2.32228751e-16,
+                        -8.22353985e-17,
+                        8.33333333e-02,
+                        0.00000000e00,
+                    ],
+                    [
+                        2.14650429e-16,
+                        -2.41327387e-16,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        5.28559454e-02,
+                    ],
+                ]
+            ),
+            6,
+        )
 
-        np.testing.assert_almost_equal(stiff.getValues(range(6),range(6)), 
-                        np.array([[ 3.11064401e-01, -5.76267647e-07,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00,  2.19833734e-16],
-                                [-5.76267647e-07,  3.11064401e-01,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00, -2.66259961e-16],
-                                [ 0.00000000e+00,  0.00000000e+00,  1.00000000e+00, 5.13607660e-16, -3.11112792e-16,  0.00000000e+00],
-                                [ 0.00000000e+00,  0.00000000e+00,  4.93713695e-16, 8.33333333e-02, -8.92869184e-17,  0.00000000e+00],
-                                [ 0.00000000e+00,  0.00000000e+00, -2.32228751e-16, -8.22353985e-17,  8.33333333e-02,  0.00000000e+00],
-                                [ 2.14650429e-16, -2.41327387e-16,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00,  5.28559454e-02]]),6)
-
-        np.testing.assert_almost_equal(mass.getValues(range(6),range(6)), 
-                        np.array([[ 1.00000000e+00,  0.00000000e+00,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00,  3.64291930e-17],
-                                [ 0.00000000e+00,  1.00000000e+00,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00, -1.30104261e-17],
-                                [ 0.00000000e+00,  0.00000000e+00,  1.00000000e+00, -3.64291930e-17,  1.30104261e-17,  0.00000000e+00],
-                                [ 0.00000000e+00,  0.00000000e+00, -3.64291930e-17, 8.33333333e-02, -2.60208521e-18,  0.00000000e+00],
-                                [ 0.00000000e+00,  0.00000000e+00,  1.30104261e-17, -2.60208521e-18,  8.33333333e-02,  0.00000000e+00],
-                                [ 3.64291930e-17, -1.30104261e-17,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00,  1.66666667e-01]]),6)
+        np.testing.assert_almost_equal(
+            mass.getValues(range(6), range(6)),
+            np.array(
+                [
+                    [
+                        1.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        3.64291930e-17,
+                    ],
+                    [
+                        0.00000000e00,
+                        1.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        -1.30104261e-17,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        1.00000000e00,
+                        -3.64291930e-17,
+                        1.30104261e-17,
+                        0.00000000e00,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        -3.64291930e-17,
+                        8.33333333e-02,
+                        -2.60208521e-18,
+                        0.00000000e00,
+                    ],
+                    [
+                        0.00000000e00,
+                        0.00000000e00,
+                        1.30104261e-17,
+                        -2.60208521e-18,
+                        8.33333333e-02,
+                        0.00000000e00,
+                    ],
+                    [
+                        3.64291930e-17,
+                        -1.30104261e-17,
+                        0.00000000e00,
+                        0.00000000e00,
+                        0.00000000e00,
+                        1.66666667e-01,
+                    ],
+                ]
+            ),
+            6,
+        )
 
 
 def suite():
