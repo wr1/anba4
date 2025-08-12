8a5ba27007e753863bb8172bbb71de55fc3433ee
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-12 15:47:04 +0200

    Updated imports for split files

--- anba4/__init__.py
+++ anba4/__init__.py
@@ -1,4 +1,13 @@
-from anba4.anbax import anbax
-from anba4.anbax_singular import anbax_singular
-from anba4 import (voight_notation, material)
+from anba4.anba_model import initialize_anba_model
+from anba4.fe_basis import initialize_fe_basis
+from anba4.solvers import compute_stiffness, compute_inertia, stress_field, strain_field
+from anba4.anba_model_singular import initialize_anba_model_singular
+from anba4.fe_basis_singular import initialize_fe_basis_singular
+from anba4.solvers_singular import (
+    compute_stiffness_singular,
+    compute_inertia_singular,
+    stress_field_singular,
+    strain_field_singular,
+)
+from anba4 import voight_notation, material
 from anba4.utils import *
